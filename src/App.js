3ce28e8042351d09d8bb74ad597443213f2a4b4a
Elze Hamilton <elze.hamilton@gmail.com>
2020-08-09 15:24:23 -0500

    Adding serverless API and calling it from the frontend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-		<p>{ (new Date()).toString() } </p>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {currentDate: (new Date('2020-08-08')).toString()};
+	}  
+
+	getDate = async () => {
+	const response = await fetch('/api/date');
+	console.log(`getDate: returned from fetch`);
+	const body = await response.json();
+	console.log(`getDate: got response.json() ; response.status = ${response.status}`);
+	if (response.status !== 200) {
+		throw Error(body.message);
+	}
+	return body;
+	};	
+
+	componentDidMount() {
+	this.getDate()
+	  .then(res => {
+		console.log(`componentDidMount: res = ${res}`);
+		this.setState({ currentDate: res })
+		//console.log(`componentDidMount: this.props.currentDate = ${this.props.currentDate}`);
+	  })
+	  .catch(err => console.log(err));
+	}  
+  
+	render() {	
+	
+	  return (
+		<div className="App">
+		  <header className="App-header">
+			<p>{ this.state.currentDate } </p>	
+		  </header>
+		</div>
+	  );
+	}
 }
 
 export default App;
